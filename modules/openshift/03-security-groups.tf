3a2db4e48f8fa23ebc4991e1590cad5f65b7dac8
Alex Kretzschmar <alexktz@gmail.com>
2018-03-12 22:58:21 +0100

    Modified for uniqueness with cluster_name and Project for Cluster tags

--- modules/openshift/03-security-groups.tf
+++ modules/openshift/03-security-groups.tf
@@ -1,9 +1,9 @@
 //  This security group allows intra-node communication on all ports with all
 //  protocols.
-resource "aws_security_group" "openshift-vpc" {
-  name        = "openshift-vpc"
+resource "aws_security_group" "ocp-vpc-${var.cluster_name}" {
+  name        = "ocp-vpc-${var.cluster_name}"
   description = "Default security group that allows all instances in the VPC to talk to each other over any port and protocol."
-  vpc_id      = "${aws_vpc.openshift.id}"
+  vpc_id      = "${aws_vpc.ocp-${var.cluster_name}.id}"
 
   ingress {
     from_port = "0"
@@ -20,17 +20,18 @@ resource "aws_security_group" "openshift-vpc" {
   }
 
   tags {
-    Name    = "OpenShift Internal VPC"
-    Project = "openshift"
+    Name    = "ocp ${var.cluster_name} Internal VPC"
+    Cluster = "ocp-${var.cluster_name}"
   }
 }
 
 //  This security group allows public ingress to the instances for HTTP, HTTPS
 //  and common HTTP/S proxy ports.
-resource "aws_security_group" "openshift-public-ingress" {
-  name        = "openshift-public-ingress"
+### TODO: Lock down to only infra nodes
+resource "aws_security_group" "ocp-public-ingress-${var.cluster_name}" {
+  name        = "ocp-public-ingress-${var.cluster_name}"
   description = "Security group that allows public ingress to instances, HTTP, HTTPS and more."
-  vpc_id      = "${aws_vpc.openshift.id}"
+  vpc_id      = "${aws_vpc.ocp-${var.cluster_name}.id}"
 
   //  HTTP
   ingress {
@@ -65,17 +66,17 @@ resource "aws_security_group" "openshift-public-ingress" {
   }
 
   tags {
-    Name    = "OpenShift Public Access"
-    Project = "openshift"
+    Name    = "ocp ${var.cluster_name} Public Access"
+    Cluster = "ocp-${var.cluster_name}"
   }
 }
 
 //  This security group allows public egress from the instances for HTTP and
 //  HTTPS, which is needed for yum updates, git access etc etc.
-resource "aws_security_group" "openshift-public-egress" {
-  name        = "openshift-public-egress"
+resource "aws_security_group" "ocp-public-egress-${var.cluster_name}" {
+  name        = "ocp-public-egress-${var.cluster_name}"
   description = "Security group that allows egress to the internet for instances over HTTP and HTTPS."
-  vpc_id      = "${aws_vpc.openshift.id}"
+  vpc_id      = "${aws_vpc.ocp-${var.cluster_name}.id}"
 
   //  HTTP
   egress {
@@ -94,16 +95,16 @@ resource "aws_security_group" "openshift-public-egress" {
   }
 
   tags {
-    Name    = "OpenShift Public Access"
-    Project = "openshift"
+    Name    = "ocp ${var.cluster_name} Public Access"
+    Cluster = "ocp-${var.cluster_name}"
   }
 }
 
 //  Security group which allows SSH access to a host. Used for the bastion.
-resource "aws_security_group" "openshift-ssh" {
-  name        = "openshift-ssh"
+resource "aws_security_group" "ocp-ssh-${var.cluster_name}" {
+  name        = "ocp-ssh-${var.cluster_name}"
   description = "Security group that allows public ingress over SSH."
-  vpc_id      = "${aws_vpc.openshift.id}"
+  vpc_id      = "${aws_vpc.ocp-${var.cluster_name}.id}"
 
   //  SSH
   ingress {
@@ -114,7 +115,7 @@ resource "aws_security_group" "openshift-ssh" {
   }
 
   tags {
-    Name    = "OpenShift SSH Access"
-    Project = "openshift"
+    Name    = "ocp ${var.cluster_name} SSH Access"
+    Cluster = "ocp-${var.cluster_name}"
   }
 }
