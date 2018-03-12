05ae2d0e49d7bd93d1700add829d54d859bf7d45
Alex Kretzschmar <alexktz@gmail.com>
2018-03-12 22:54:32 +0100

    Modified VPC related resources for uniqueness

--- modules/openshift/02-vpc.tf
+++ modules/openshift/02-vpc.tf
@@ -1,56 +1,56 @@
 //  Define the VPC.
-resource "aws_vpc" "openshift" {
+resource "aws_vpc" "ocp-${var.cluster_name}" {
   cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = true
 
   tags {
-    Name    = "OpenShift VPC"
-    Project = "openshift"
+    Name    = "ocp ${var.cluster_name} VPC"
+    Project = "ocp-${var.cluster_name}"
   }
 }
 
 //  Create an Internet Gateway for the VPC.
-resource "aws_internet_gateway" "openshift" {
-  vpc_id = "${aws_vpc.openshift.id}"
+resource "aws_internet_gateway" "ocp-${var.cluster_name}" {
+  vpc_id = "${aws_vpc.ocp.id}"
 
   tags {
-    Name    = "OpenShift IGW"
-    Project = "openshift"
+    Name    = "ocp ${var.cluster_name} IGW"
+    Project = "ocp-${var.cluster_name}"
   }
 }
 
 //  Create a public subnet.
-resource "aws_subnet" "public-subnet" {
-  vpc_id                  = "${aws_vpc.openshift.id}"
+resource "aws_subnet" "public-subnet-${var.cluster_name}" {
+  vpc_id                  = "${aws_vpc.ocp.id}"
   cidr_block              = "${var.subnet_cidr}"
   availability_zone       = "${lookup(var.subnetaz, var.region)}"
   map_public_ip_on_launch = true
-  depends_on              = ["aws_internet_gateway.openshift"]
+  depends_on              = ["aws_internet_gateway.ocp"]
 
   tags {
-    Name    = "OpenShift Public Subnet"
-    Project = "openshift"
+    Name    = "OCP ${var.cluster_name} Public Subnet"
+    Project = "ocp"
   }
 }
 
 //  Create a route table allowing all addresses access to the IGW.
-resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.openshift.id}"
+resource "aws_route_table" "public-${var.cluster_name}" {
+  vpc_id = "${aws_vpc.ocp.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.openshift.id}"
+    gateway_id = "${aws_internet_gateway.ocp.id}"
   }
 
   tags {
-    Name    = "OpenShift Public Route Table"
-    Project = "openshift"
+    Name    = "OCP ${var.cluster_name} Public Route Table"
+    Project = "ocp"
   }
 }
 
 //  Now associate the route table with the public subnet - giving
 //  all public subnet instances access to the internet.
-resource "aws_route_table_association" "public-subnet" {
-  subnet_id      = "${aws_subnet.public-subnet.id}"
-  route_table_id = "${aws_route_table.public.id}"
+resource "aws_route_table_association" "public-subnet-${var.cluster_name}" {
+  subnet_id      = "${aws_subnet.public-subnet-${var.cluster_name}.id}"
+  route_table_id = "${aws_route_table.public-${var.cluster_name}.id}"
 }
